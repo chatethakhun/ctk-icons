72b4165ad8836d7ad175a9d97d18b5ee0f4e1f88
Chatethakhun Khongthon <chatethakhunkhongthon@Chatethakhuns-MacBook-Air.local>
2025-07-23 11:45:51 +0700

    remove node_modules

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import * as react_jsx_runtime from 'react/jsx-runtime';
-import { SVGProps } from 'react';
-
-declare const SvgFileCheckIcon: (props: SVGProps<SVGSVGElement>) => react_jsx_runtime.JSX.Element;
-
-declare const SvgSettingsIcon: (props: SVGProps<SVGSVGElement>) => react_jsx_runtime.JSX.Element;
-
-export { SvgFileCheckIcon as FileCheckIcon, SvgSettingsIcon as SettingsIcon };
